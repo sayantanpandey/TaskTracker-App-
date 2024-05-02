d1b1e01c04ccb30fa24d5e06efdd84554c5e266f
Sayantan Pandey <125211013+sayantanpandey@users.noreply.github.com>
2024-05-02 12:41:31 +0530

    commit

--- src/component/TaskTrackerApp.jsx
+++ src/component/TaskTrackerApp.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
   AppBar,
   Toolbar,
@@ -11,14 +11,21 @@ import {
   IconButton,
   Stack,
   useMediaQuery,
-} from '@mui/material';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Brightness4 as DarkModeIcon, Brightness7 as LightModeIcon } from '@mui/icons-material';
-import TaskForm from './TaskForm';
-import TaskList from './TaskList';
-import CompletedTasksTab from './CompletedTasksTab';
-import SettingsTab from './SettingsTab';
-import { CheckCircleOutline as CheckCircleOutlineIcon, PlaylistAddCheck as PlaylistAddCheckIcon, Settings as SettingsIcon } from '@mui/icons-material';
+} from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import {
+  Brightness4 as DarkModeIcon,
+  Brightness7 as LightModeIcon,
+} from "@mui/icons-material";
+import TaskForm from "./TaskForm";
+import TaskList from "./TaskList";
+import CompletedTasksTab from "./CompletedTasksTab";
+import SettingsTab from "./SettingsTab";
+import {
+  CheckCircleOutline as CheckCircleOutlineIcon,
+  PlaylistAddCheck as PlaylistAddCheckIcon,
+  Settings as SettingsIcon,
+} from "@mui/icons-material";
 const TaskTrackerApp = () => {
   const [tasks, setTasks] = useState([]);
   const [completedTasks, setCompletedTasks] = useState([]);
@@ -26,22 +33,22 @@ const TaskTrackerApp = () => {
   const [darkMode, setDarkMode] = useState(false);
 
   useEffect(() => {
-    const storedTasks = localStorage.getItem('tasks');
+    const storedTasks = localStorage.getItem("tasks");
     if (storedTasks) {
       setTasks(JSON.parse(storedTasks));
     }
-    const storedCompletedTasks = localStorage.getItem('completedTasks');
+    const storedCompletedTasks = localStorage.getItem("completedTasks");
     if (storedCompletedTasks) {
       setCompletedTasks(JSON.parse(storedCompletedTasks));
     }
   }, []);
 
   useEffect(() => {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+    localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
 
   useEffect(() => {
-    localStorage.setItem('completedTasks', JSON.stringify(completedTasks));
+    localStorage.setItem("completedTasks", JSON.stringify(completedTasks));
   }, [completedTasks]);
 
   const handleAddTask = (newTask) => {
@@ -78,97 +85,145 @@ const TaskTrackerApp = () => {
   };
 
   const theme = createTheme({
-  palette: {
-    mode: darkMode ? 'dark' : 'light',
-    primary: {
-      main: '#008080', // Teal color
+    palette: {
+      mode: darkMode ? "dark" : "light",
+      primary: {
+        main: "#008080", // Teal color
+      },
+      secondary: {
+        main: "#dc004e",
+      },
     },
-    secondary: {
-      main: '#dc004e',
-    },
-  },
-});
-
+  });
 
   const handleChangeTab = (event, newValue) => {
     setTabValue(newValue);
   };
 
-  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <AppBar position="sticky">
-  <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-    <Typography variant="h4" sx={{ fontWeight: 'bold', textAlign: 'center', width: '100%' }}>
-      Task Tracker
-    </Typography>
-    <IconButton onClick={handleThemeChange} sx={{ color: 'inherit' }}>
-      {darkMode ? <LightModeIcon /> : <DarkModeIcon />}
-    </IconButton>
-  </Toolbar>
-  <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
-  <Tabs value={tabValue} onChange={handleChangeTab} indicatorColor="primary" textColor="inherit">
-      <Tab
-        label={
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <PlaylistAddCheckIcon sx={{ fontSize: 30, marginRight: 1 }} /> {/* Increased icon size */}
-            Tasks
-          </Box>
-        }
-        sx={{
-          fontWeight: tabValue === 0 ? 'bold' : 'normal',
-          color: 'white',
-          '&.Mui-selected': {
-            color: 'white',
-          },
-          fontSize: 18,
-          marginRight: 10,
-        }}
-      />
-      <Tab
-        label={
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <CheckCircleOutlineIcon sx={{ marginRight: 1 }} />
+        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Typography
+            variant="h4"
+            sx={{ fontWeight: "bold", textAlign: "center", width: "100%" }}
+          >
+            Task Tracker
+          </Typography>
+          <IconButton onClick={handleThemeChange} sx={{ color: "inherit" }}>
+            {darkMode ? <LightModeIcon /> : <DarkModeIcon />}
+          </IconButton>
+        </Toolbar>
+        <Box sx={{ display: "flex", justifyContent: "center", mt: 2 }}>
+  <Tabs
+    value={tabValue}
+    onChange={handleChangeTab}
+    indicatorColor="primary"
+    textColor="inherit"
+    sx={{
+      display: "flex",
+      flexDirection: isMobile ? "column" : "row", // Change direction to column on mobile
+      alignItems: "center",
+      justifyContent: isMobile ? "center" : "space-between", // Center tabs on mobile
+    }}
+  >
+    <Tab
+      label={
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <PlaylistAddCheckIcon sx={{ fontSize: isMobile ? 18 : 24 }} /> {/* Adjusted icon size */}
+          Tasks
+        </Box>
+      }
+      sx={{
+        flex: isMobile ? "1 1 auto" : "0 1 auto", // Allow tabs to grow on mobile
+        fontWeight: tabValue === 0 ? "bold" : "normal",
+        color: "white",
+        "&.Mui-selected": {
+          color: "white",
+        },
+        fontSize: isMobile ? 12 : 16, // Adjusted font size for mobile
+         // Add space between tabs on mobile
+        marginRight: isMobile ? 0 : 10, // Adjust margin for tabs on mobile
+      }}
+    />
+    <Tab
+      label={
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <CheckCircleOutlineIcon
+            sx={{
+              fontSize: isMobile ? 14 : 20,
+              marginRight: isMobile ? 0.5 : 1,
+            }}
+          />
+          <span style={{ marginLeft: isMobile ? -1 : 3 }}>
+            {/* Adjusted margin for mobile */}
             Completed Tasks
-            <span style={{ marginLeft: 5, fontSize: 18, fontWeight: 'bold' }}>
+            <span
+              style={{
+                marginLeft: 3,
+                fontSize: isMobile ? 12 : 16,
+                fontWeight: "bold",
+              }}
+            >
               ({completedTasks.length})
             </span>
-          </Box>
-        }
-        sx={{
-          fontWeight: tabValue === 1 ? 'bold' : 'normal',
-          color: 'white',
-          '&.Mui-selected': {
-            color: 'white',
-          },
-          fontSize: 18,
-          marginRight: 10,
-        }}
-      />
-      <Tab
-        label={
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <SettingsIcon sx={{ marginRight: 1 }} />
-            Settings
-          </Box>
-        }
-        sx={{
-          fontWeight: tabValue === 2 ? 'bold' : 'normal',
-          color: 'white',
-          '&.Mui-selected': {
-            color: 'white',
-          },
-          fontSize: 18,
-          marginRight: 10,
-        }}
-      />
-    </Tabs>
+          </span>
+        </Box>
+      }
+      sx={{
+        flex: isMobile ? "1 1 auto" : "0 1 auto", // Allow tabs to grow on mobile
+        fontWeight: tabValue === 1 ? "bold" : "normal",
+        color: "white",
+        "&.Mui-selected": {
+          color: "white",
+        },
+        fontSize: isMobile ? 12 : 16, // Adjusted font size for mobile
+        // Add space between tabs on mobile
+        marginRight: isMobile ? 0 : 10, // Adjust margin for tabs on mobile
+      }}
+    />
+
+    <Tab
+      label={
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <SettingsIcon
+            sx={{
+              fontSize: isMobile ? 14 : 20,
+              marginRight: isMobile ? 0 : 1,
+            }}
+          />
+          Settings
+        </Box>
+      }
+      sx={{
+        flex: isMobile ? "1 1 auto" : "0 1 auto", // Allow tabs to grow on mobile
+        fontWeight: tabValue === 2 ? "bold" : "normal",
+        color: "white",
+        "&.Mui-selected": {
+          color: "white",
+        },
+        fontSize: isMobile ? 12 : 16, // Adjusted font size for mobile
+        // Add space between tabs on mobile
+        marginRight: isMobile ? 0 : 10, // Adjust margin for tabs on mobile
+      }}
+    />
+  </Tabs>
 </Box>
-</AppBar>
+
+      </AppBar>
       <Container sx={{ mt: 8 }}>
-        <Box sx={{ bgcolor: darkMode ? '#333' : '#f4f4f4', p: 3, borderRadius: 8 }}>
+        <Box
+          sx={{ bgcolor: darkMode ? "#333" : "#f4f4f4", p: 3, borderRadius: 8 }}
+        >
           {tabValue === 0 && (
             <>
               <TaskForm onAddTask={handleAddTask} />
@@ -181,10 +236,16 @@ const TaskTrackerApp = () => {
             </>
           )}
           {tabValue === 1 && (
-            <CompletedTasksTab tasks={completedTasks} onDeleteCompletedTask={handleDeleteCompletedTask} />
+            <CompletedTasksTab
+              tasks={completedTasks}
+              onDeleteCompletedTask={handleDeleteCompletedTask}
+            />
           )}
           {tabValue === 2 && (
-            <SettingsTab darkMode={darkMode} onThemeChange={handleThemeChange} />
+            <SettingsTab
+              darkMode={darkMode}
+              onThemeChange={handleThemeChange}
+            />
           )}
         </Box>
       </Container>
